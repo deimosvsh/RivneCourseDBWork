5174b2349e9c1eed2ac6c0495531c9dbc127eb7c
volempdoge <volempdoge@gmail.com>
2022-07-13 18:53:36 +0300

    2

--- CourseDBWork/AddEx.Designer.cs
+++ CourseDBWork/AddEx.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
             this.button6 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -40,11 +44,16 @@
             this.label10 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.numericUpDown1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.dateTimePicker1);
+            this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Controls.Add(this.button6);
             this.tabPage2.Controls.Add(this.button4);
             this.tabPage2.Controls.Add(this.button3);
@@ -63,6 +72,41 @@
             this.tabPage2.Text = "Виставка";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(27, 243);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 42;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 213);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 17);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Хол";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(27, 134);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(344, 22);
+            this.dateTimePicker1.TabIndex = 39;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 115);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Дата кінця";
+            // 
             // button6
             // 
             this.button6.Location = new System.Drawing.Point(287, 486);
@@ -72,6 +116,7 @@
             this.button6.TabIndex = 37;
             this.button6.Text = "Редагувати";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button4
             // 
@@ -82,6 +127,7 @@
             this.button4.TabIndex = 36;
             this.button4.Text = "Додати";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
@@ -92,11 +138,12 @@
             this.button3.TabIndex = 35;
             this.button3.Text = "Очистити";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(27, 258);
+            this.comboBox1.Location = new System.Drawing.Point(27, 180);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(344, 24);
@@ -104,7 +151,7 @@
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(27, 188);
+            this.dateTimePicker2.Location = new System.Drawing.Point(26, 85);
             this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(344, 22);
@@ -113,16 +160,16 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(23, 169);
+            this.label6.Location = new System.Drawing.Point(22, 66);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(117, 17);
+            this.label6.Size = new System.Drawing.Size(99, 17);
             this.label6.TabIndex = 30;
-            this.label6.Text = "Дата написання";
+            this.label6.Text = "Дата початку";
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(27, 126);
+            this.textBox8.Location = new System.Drawing.Point(27, 38);
             this.textBox8.Margin = new System.Windows.Forms.Padding(4);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(344, 22);
@@ -131,23 +178,22 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(23, 239);
+            this.label9.Location = new System.Drawing.Point(23, 161);
             this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(72, 17);
+            this.label9.Size = new System.Drawing.Size(64, 17);
             this.label9.TabIndex = 27;
-            this.label9.Text = "Художник";
-            this.label9.Click += new System.EventHandler(this.label9_Click);
+            this.label9.Text = "Картини";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(23, 106);
+            this.label10.Location = new System.Drawing.Point(23, 18);
             this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(106, 17);
+            this.label10.Size = new System.Drawing.Size(105, 17);
             this.label10.TabIndex = 26;
-            this.label10.Text = "Назва картини";
+            this.label10.Text = "Тема виставки";
             // 
             // tabControl1
             // 
@@ -173,6 +219,7 @@
             this.Text = "AddEx";
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -191,5 +238,9 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- CourseDBWork/AddEx.cs
+++ CourseDBWork/AddEx.cs
@@ -28,7 +28,12 @@ namespace CourseDBWork
             EditableID = EID;
             dtPictures = ConnDB.TryQuery("SELECT * FROM [pictures]");
 
-            //DataTable LoadHelper = ConnDB.TryQuery(String.Format("select * from painters where id_painter = '{0}'", EditableID));
+            DataTable LoadHelper = ConnDB.TryQuery(String.Format("select * from painters where id_painter = '{0}'", EditableID));
+                        
+                //textBox1.Text = LoadHelper.Rows[0][1].ToString();
+                //textBox4.Text = LoadHelper.Rows[0][3].ToString();
+                dateTimePicker1.Value = DateTime.Parse(LoadHelper.Rows[0][1].ToString());
+                dateTimePicker2.Value = DateTime.Parse(LoadHelper.Rows[0][2].ToString());
         }
         private void AddForm_Load(object sender, EventArgs e)
         {
@@ -44,9 +49,38 @@ namespace CourseDBWork
             comboBox1.SelectedItem = comboBox1.Items[0];
         }
 
-        private void label9_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
+            //TODO
+            // додати виставку
+            string tmp = textBox8.Text;
+            string formatter = dateTimePicker2.Value.Date.ToString("yyyy-MM-dd");
+            string paintId = "";
+            string techID = "";
+            string typeID = "";
+            foreach (var i in Pictures)
+                if (i.Value == comboBox1.SelectedItem.ToString()) paintId = i.Key.ToString();
+            ConnDB.TryQuery(String.Format("INSERT INTO[dbo].[painting]([pic_name],[pic_date],[id_painter],[id_tech],[id_type]) VALUES('{0}', '{1}', '{2}', '{3}', '{4}');",
+            textBox8.Text.Replace("'", "`"), formatter, paintId, techID, typeID));
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox8.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //TODO
+            // додати виставку
+            string tmp = textBox8.Text;
+            string formatter = dateTimePicker1.Value.Date.ToString("yyyy-MM-dd");
+            ConnDB.TryQuery(String.Format("UPDATE[dbo].[painters] SET [painter_name] = '{0}', [painter_birth] = '{1}', [painter_genre] = '{2}' WHERE[dbo].[painters].[id_painter] = '{3}';",
+                                                                     tmp.Replace("'", "`"), formatter, textBox8.Text.Replace("'", "`"), EditableID));
+            this.DialogResult = DialogResult.OK;
+            this.Close();
         }
     }
 }
